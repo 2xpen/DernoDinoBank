5be837bf839d0d79d49239198a54de605c6bcc16
2xpen <149803952+2xpen@users.noreply.github.com>
2024-12-19 12:00:13 +0100

    jahu

--- src/test/java/serviceTest/ImportExportTest.java
+++ src/test/java/serviceTest/ImportExportTest.java
@@ -1,13 +1,27 @@
 package serviceTest;
 
+import csv.CSV_Handler;
+import csv.ICSV_IMPORT_EXPORT;
 import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
+import service.ImportExportService;
+import service.KontoService;
+import service.UserService;
 
 public class ImportExportTest {
 
+    ICSV_IMPORT_EXPORT csvHandler = new CSV_Handler();
+    private UserService userService;
+    private KontoService kontoService;
+    private ImportExportService importExportService;
+
+
     @BeforeEach
     public void setUp() {
+        userService = Mockito.mock(UserService.class);
+        kontoService = Mockito.mock(KontoService.class);
 
+        importExportService = new ImportExportService(userService, kontoService);
     }
-
 
 }
--- src/test/java/serviceTest/RegistrieungServiceTest.java
+++ src/test/java/serviceTest/RegistrieungServiceTest.java
@@ -1,6 +1,66 @@
 package serviceTest;
 
+import data.user.Passwort;
+import data.user.UserName;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import repository.UserRepository;
+import repository.konto.KontoRepository;
+import service.RegistrierungService;
+import service.serviceexception.RegistrierungException;
+import service.serviceexception.ServiceException;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 public class RegistrieungServiceTest {
+
+    private UserRepository userRepository;
+    private KontoRepository kontoRepository;
+
+    private RegistrierungService registrierungService;
+
+    @BeforeEach
+    public void setUp() {
+        userRepository = Mockito.mock(UserRepository.class);
+        kontoRepository = Mockito.mock(KontoRepository.class);
+
+        registrierungService = new RegistrierungService(userRepository, kontoRepository);
+    }
+
+
+    @Test
+    void testRegistrierungPositiv() throws SQLException, ServiceException {
+
+        UserName userName = new UserName("valid@hsw-stud.de");
+        Passwort password = new Passwort("validPasswort");
+
+
+        when(userRepository.userNameExists(userName)).thenReturn(false);
+
+        assertDoesNotThrow(() -> registrierungService.registrieren(userName, password));
+
+    }
+
+
+    @Test
+    void testRegistrierungBenutzernameVergeben() throws SQLException, ServiceException {
+
+        UserName userName = new UserName("schonVergeben@hsw-stud.de");
+        Passwort password = new Passwort("validPasswort");
+
+
+        when(userRepository.userNameExists(userName)).thenReturn(true);
+
+        RegistrierungException e =
+                assertThrows(RegistrierungException.class, () -> registrierungService.registrieren(userName, password));
+
+        assertEquals(RegistrierungException.Message.BENUTZERNAME_BEREITS_VERGEBEN.getServiceErrorMessage(), e.getMessage());
+
+    }
 
 
 }
